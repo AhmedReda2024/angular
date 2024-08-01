d479f1226497435d0591698915e54f95b6bbb123
AhmedReda2024 <ahmedreda>
2024-08-01 21:14:40 +0300

    uploading

---
+++ docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-3IWSJ6YJ.mjs";import"./chunk-AKVWXP62.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
---
+++ src/app/portfolio/portfolio.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portfolio',
+  standalone: true,
+  imports: [],
+  templateUrl: './portfolio.component.html',
+  styleUrl: './portfolio.component.css',
+})
+export class PortfolioComponent {
+  isHidden: boolean = true;
+  modelImg: string = '';
+  imgSrc: string[] = [
+    './assets/imgs/poert1.png',
+    './assets/imgs/port2.png',
+    './assets/imgs/port3.png',
+    './assets/imgs/port3.png',
+    './assets/imgs/poert1.png',
+    './assets/imgs/port2.png',
+  ];
+
+  hideModel(eleTarget: EventTarget | null, eleRef: HTMLImageElement): void {
+    if (eleTarget == eleRef) return;
+    else {
+      this.isHidden = true;
+    }
+  }
+}
